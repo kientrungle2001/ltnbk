16dd247847454840f56f53334290428408a7915a
kieunghia121310 <kieunghia.cntt@gmail.com>
2018-10-30 14:44:05 +0700

    .js

--- common/footer.php
+++ common/footer.php
@@ -39,7 +39,19 @@
 			</div>
 		</div>
 	</div>
-	
+	<!--Start of Tawk.to Script-->
+<script type="text/javascript">
+var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+(function(){
+var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+s1.async=true;
+s1.src='https://embed.tawk.to/56d905db5ef21b26679a3cfc/default';
+s1.charset='UTF-8';
+s1.setAttribute('crossorigin','*');
+s0.parentNode.insertBefore(s1,s0);
+})();
+</script>
+<!--End of Tawk.to Script-->
 	<!--img class="img-fluid" src="/assets/images/footer.png" />
 	<div class="footer-bottom text-center">
 		Bản quyền thuộc về Công Ty Cổ Phần Giáo Dục Phát Triển Trí Tuệ 
